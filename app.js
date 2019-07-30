dea6fa319ec5871a084fc02ae4dcb8339018eede
Root <forte@mailinator.com>
2019-07-30 15:42:30 +0530

    update app.js

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-import React, { Component } from 'react';
-class App extends Component{
-   render(){
-      return(
-         <div>
-            <h1>Hello World</h1>
-         </div>
-      );
-   }
-}
-export default App;
+const http = require('http');
+const port = process.env.PORT || 3000
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Hello World</h1>');
+});
+
+server.listen(port,() => {
+  console.log(`Server running at port `+port);
+});
