26c81d8d33fa6baa97bc5a4d61edaaf07ed57944
Root <forte@mailinator.com>
2019-07-31 13:18:44 +0530

    update contact-us.js

--- contact-us.js
+++ contact-us.js
@@ -11,6 +11,7 @@ if ( !url ) {
 
     const browser = await puppeteer.launch({headless: false,args: ['--no-sandbox', '--disable-setuid-sandbox'],ignoreDefaultArgs: ['--disable-extensions']});
     const page = await browser.newPage();
+    await page.setUserAgent('Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36');
     await page.setViewport({ width: 1280, height: 800 })
     await page.goto(url, {waitUntil: 'networkidle2'});
 
