9f03ec3adfee9e0130e45515004b13987e48e880
Root <forte@mailinator.com>
2019-08-01 14:48:37 +0530

    update demo.js

--- demo.js
+++ demo.js
@@ -3,7 +3,7 @@ var data = require('./data.json');
 
 async function run() {  
 
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
     const page = await browser.newPage();
     await page.setViewport({ width: 1280, height: 800 })
     await page.goto("https://reflektivedev.wpengine.com/contact/", {waitUntil: 'networkidle2'});
