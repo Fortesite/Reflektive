305b23e84ceb13e23573f17d108033c42ba994da
Root <forte@mailinator.com>
2019-08-01 16:16:22 +0530

    update demo.js

--- demo.js
+++ demo.js
@@ -42,7 +42,7 @@ if (await page.$('#Phone') !== null){
     }
     
     await page.screenshot({
-    path: '/Screenshots/demo.jpg',
+    path: 'demo.jpg',
     fullPage: true
     });
      if (await page.$('.mktoButton') !== null){
@@ -53,7 +53,7 @@ if (await page.$('#Phone') !== null){
     console.log('Form Submitted');
     await page.waitFor(1000);
     await page.screenshot({
-    path: '/Screenshots/demo-click.jpg',
+    path: 'demo-click.jpg',
     fullPage: true
     });
     await browser.close();
